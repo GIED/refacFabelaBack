626060151cd63d9353117fad4be2b6f991e9edc4
Pedro Fabela <47532745+pedrofabela@users.noreply.github.com>
2024-11-22 17:04:47 -0600

    mas del modulo de mau

--- refacFabela/src/main/java/com/refacFabela/repository/VwProductoMetaCompraRepository.java
+++ refacFabela/src/main/java/com/refacFabela/repository/VwProductoMetaCompraRepository.java
@@ -11,7 +11,7 @@ import com.refacFabela.model.VwProductoMetaCompra;
 @Repository
 public interface VwProductoMetaCompraRepository extends JpaRepository<VwProductoMetaCompra, Long> {
 	
-	@Query(value = "SELECT * FROM vw_producto_meta_compra c WHERE c.d_ultima_fecha_compra >= STR_TO_DATE(:fechaInicio, '%Y-%m-%d') AND c.d_ultima_fecha_compra <= STR_TO_DATE(:fechaTermino, '%Y-%m-%d')", nativeQuery = true)
+	@Query(value = "SELECT * FROM vw_producto_meta_compra c WHERE c.d_ultima_fecha_compra >= STR_TO_DATE(:fechaInicio, '%Y-%m-%d') AND c.d_ultima_fecha_compra <= STR_TO_DATE(:fechaTermino, '%Y-%m-%d' ) order by n_cantidad asc", nativeQuery = true)
 	public List<VwProductoMetaCompra> ultimafechaCompra(String fechaInicio, String fechaTermino);
 
 }
