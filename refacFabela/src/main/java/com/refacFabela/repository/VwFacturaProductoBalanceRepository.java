37a5eca839c5651cd9612e561c0118717b1b8958
Pedro Fabela <47532745+pedrofabela@users.noreply.github.com>
2025-03-24 16:57:20 -0600

    se cambia la consulta para que solo traiga facturas de provedores de productos.

--- refacFabela/src/main/java/com/refacFabela/repository/VwFacturaProductoBalanceRepository.java
+++ refacFabela/src/main/java/com/refacFabela/repository/VwFacturaProductoBalanceRepository.java
@@ -9,7 +9,7 @@ import com.refacFabela.model.VwFacturaProductoBalance;
 
 public interface VwFacturaProductoBalanceRepository extends JpaRepository<VwFacturaProductoBalance, Long> {
 	
-	@Query("Select c from VwFacturaProductoBalance c where c.nEstatusIngresoAlmacen=:nEstatusAlmacen order by c.nId desc ")
+	@Query("Select c from VwFacturaProductoBalance c where c.nEstatusIngresoAlmacen=:nEstatusAlmacen and c.tcProveedor.nIdTipoProveedor=1 order by c.nId desc ")
 	public List<VwFacturaProductoBalance> obtenerFacturasEstatusAlmacen(Integer nEstatusAlmacen);
 
 }
