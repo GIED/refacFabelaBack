39358aca63fbda2dc157b6f3d79c9bbe61b24f0f
pedro <fabelapedro@gmail.com>
2026-03-09 22:29:03 -0600

    se mejora para que se muestre la facturacion

--- refacFabela/src/main/java/com/refacFabela/RefacFabelaApplication.java
+++ refacFabela/src/main/java/com/refacFabela/RefacFabelaApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 public class RefacFabelaApplication {
 
 	public static void main(String[] args) {
+		// Fix JAX-WS provider for JDK 9+ (Spring Boot classloader doesn't discover META-INF/services from nested JARs)
+		System.setProperty("javax.xml.ws.spi.Provider", "com.sun.xml.ws.spi.ProviderImpl");
 		SpringApplication.run(RefacFabelaApplication.class, args);
 	}
 	
