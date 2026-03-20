6e230673a6c1b2f94af371b89fabd8a1a609ce2c
Pedro Manuel Fabela Martínez <pedro.fabela@edugem.gob.mx>
2026-03-20 17:02:33 -0600

    Add initial log file for refacFabela

--- refacFabela/src/main/java/com/refacFabela/RefacFabelaApplication.java
+++ refacFabela/src/main/java/com/refacFabela/RefacFabelaApplication.java
@@ -11,6 +11,10 @@ public class RefacFabelaApplication {
 	public static void main(String[] args) {
 		// Fix JAX-WS provider for JDK 9+ (Spring Boot classloader doesn't discover META-INF/services from nested JARs)
 		System.setProperty("javax.xml.ws.spi.Provider", "com.sun.xml.ws.spi.ProviderImpl");
+		// Fix TLS handshake failures with JDK 1.8 against modern HTTPS servers (foliosdigitalespac.com)
+		// JDK 8 defaults to TLS 1.0; force TLS 1.2 to prevent SOAP connection failures returning 0 credits
+		System.setProperty("https.protocols", "TLSv1.2");
+		System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
 		SpringApplication.run(RefacFabelaApplication.class, args);
 	}
 	
